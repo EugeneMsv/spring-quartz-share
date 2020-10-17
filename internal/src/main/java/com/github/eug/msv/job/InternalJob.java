0d49780137bbc0e853a06c79030a1908afdea5f2
EugeneMsv <2cen41x7>
2020-10-17 20:09:40 +0300

    emulate job load balancing

--- internal/src/main/java/com/github/eug/msv/job/InternalJob.java
+++ internal/src/main/java/com/github/eug/msv/job/InternalJob.java
@@ -3,15 +3,15 @@ package com.github.eug.msv.job;
 import com.github.eug.msv.entity.ClusterCounter;
 import com.github.eug.msv.repository.ClusterCounterRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
-import java.time.Instant;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
@@ -21,7 +21,6 @@ import java.util.concurrent.atomic.AtomicLong;
  */
 @RequiredArgsConstructor
 @Slf4j
-
 public class InternalJob implements Job {
 
     private static AtomicLong perNodeCounter = new AtomicLong(0);
@@ -29,14 +28,23 @@ public class InternalJob implements Job {
     @Autowired
     private ClusterCounterRepository clusterCounterRepository;
 
+    @Value("${service.job.internal.time}")
+    private int jobEmulationTime;
+
     @Override
     @Transactional
+    @SneakyThrows
     public void execute(JobExecutionContext context) throws JobExecutionException {
+        long nodeCounterResult = perNodeCounter.incrementAndGet();
         ClusterCounter clusterCounter = clusterCounterRepository.findOne(1L);
-        long nodeCounterResult = perNodeCounter.getAndIncrement();
-        log.warn("InternalJob executed!!!!!!!!!! perNodeCounter={}, clusterCounter={}",
-                nodeCounterResult,
-                clusterCounter.getVersion());
+
+        log.warn("InternalJob starting perNodeCounter={}, clusterCounter={}",
+                nodeCounterResult, clusterCounter.getVersion());
+
+        Thread.sleep(jobEmulationTime);
+
+        log.warn("InternalJob finished perNodeCounter={}, clusterCounter={}",
+                nodeCounterResult, clusterCounter.getVersion());
 
         clusterCounter.setNodeCounter(nodeCounterResult);
         clusterCounterRepository.save(clusterCounter);
